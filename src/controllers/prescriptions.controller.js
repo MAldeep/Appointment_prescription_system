f2f2b719775509afc63a7a00d5f443d9863811ef
MAldeep <m7mdeldeeeep@gmail.com>
2026-07-30 14:46:03 +0300

    Prescriptions controllers

---
+++ src/controllers/prescriptions.controller.js
@@ -0,0 +1,28 @@
+import {
+  createPrescription,
+  getOnePrescription,
+} from "../services/prescriptions.service.js";
+import AppError from "../utils/appError.js";
+import catchAsync from "../utils/catchAsync.js";
+
+// create
+export const createPrescriptionCtrl = catchAsync(async (req, res, next) => {
+  const data = req.body;
+  const newPrescription = await createPrescription(data);
+  res.status(201).json({
+    success: true,
+    data: newPrescription,
+  });
+});
+// get one
+export const getOnePrescriptionCtrl = catchAsync(async (req, res, next) => {
+  const { id } = req.params;
+  const prescription = await getOnePrescription(id);
+  if (!prescription) {
+    return next(new AppError("Prescription with this id is not found", 404));
+  }
+  res.status(200).json({
+    succes: true,
+    data: prescription,
+  });
+});
